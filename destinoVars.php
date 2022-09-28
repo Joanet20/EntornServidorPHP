011f4a0706741b0a84e026e704d901f3aed7bcb0
Joan Vallbona <johnyvallbona@gmail.com>
2022-09-28 09:27:16 +0200

    Codi 'destinoVars.php'

---
+++ destinoVars.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Mostrarem es valor de ses variables pasades pes link empleant $_GET
+
+    $nom = $_GET['nom'];
+    $edat = $_GET['edat'];
+
+    echo "Nom: " . $nom;
+    echo "<p> </p>";
+    echo "Edat: " . $edat;
+
+?>
