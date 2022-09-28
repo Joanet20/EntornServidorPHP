98d298664ee0ffc02e8d5113855ffc8608832a89
Joan Vallbona <johnyvallbona@gmail.com>
2022-09-28 09:02:45 +0200

    Codi fitxer 'varsPredefinides.php'

---
+++ varsPredefinidas.php
@@ -0,0 +1,11 @@
+<?php
+
+    //Variables predefinides de PHP
+
+    echo "Nom Servidor: " . $_SERVER['SERVER_NAME'];
+    echo "<p> </p>";
+    echo "Port Servidor: " . $_SERVER['SERVER_PORT'];
+    echo "<p> </p>";
+    echo "Software Servidor: " . $_SERVER['SERVER_SOFTWARE'];
+
+?>
