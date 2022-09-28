fd76777a338c0128813ba37de4095e7a313b72c1
Joan Vallbona <johnyvallbona@gmail.com>
2022-09-28 08:40:01 +0200

    Fitxer 'variables.php' amb codi

--- variables.php
+++ variables.php
@@ -1,1 +1,12 @@
 <?php
+
+    $nom = "Joan";
+    $edat = 21;
+    $esAlumne = true;
+    $jsEsAlumne = json_encode($esAlumne);
+
+    echo "Nom: " . $nom . "<br>" .
+        "Edat: " . $edat . "<br>" .
+        "Alumne: " . $jsEsAlumne;
+
+    ?>
