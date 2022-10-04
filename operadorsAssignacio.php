411f23725a0ef8f41b2063f557ec67ccd4e95ea1
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-04 09:40:37 +0200

    Fitxer 'operadorsAsignacio.php' amb codi

---
+++ operadorsAssignacio.php
@@ -0,0 +1,27 @@
+<?php
+    $num1 = 24;
+    $num2 = 4;
+    $num3 = 0;
+
+    $num3 = $num1;
+    echo "num3 = num1 " . $num3;
+    echo "<p> </p>";
+
+    echo $num1 . " += " . $num2 . " -> " . $num1 += $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " -= " . $num2 . " -> " . $num1 -= $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " *= " . $num2 . " -> " . $num1 *= $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " /= " . $num2 . " -> " . $num1 /= $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " %= " . $num2 . " -> " . $num1 %= $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " **= " . $num2 . " -> " . $num1 **= $num2;
+    echo "<p> </p>";
+?>
