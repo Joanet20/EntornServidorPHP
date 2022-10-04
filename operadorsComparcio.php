700ccaa93b7f23fb72b4dfbe699a5216b4198a88
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-04 09:48:32 +0200

    Fitxer 'operadorsComparacio.php' amb codi

---
+++ operadorsComparcio.php
@@ -0,0 +1,36 @@
+<?php
+    $num1 = 24;
+    $num2 = 4;
+
+    if ($num1 == $num2){
+        echo "num1 és igual a num2";
+    }
+
+    if ($num1 === $num2){
+        echo "num1 és idèntic a num2";
+    }
+
+    if ($num1 != $num2){
+        echo "num1 és diferent a num2";
+    }
+
+    if ($num1 !== $num2){
+        echo "num1 no és identic a num2";
+    }
+
+    if ($num1 > $num2){
+        echo "num1 és major que num2";
+    }
+
+    if ($num1 < $num2){
+        echo "num1 és menor que num2";
+    }
+
+    if ($num1 >= $num2){
+        echo "num1 és major o igual que num2";
+    }
+
+    if ($num1 <= $num2){
+        echo "num1 és menor o igual que num2";
+    }
+?>
