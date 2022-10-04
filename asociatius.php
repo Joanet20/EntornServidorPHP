322c16788b830a6b5b8495633753adaf3400f066
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-04 08:44:26 +0200

    Fitxer 'asociatius.php' completat

--- asociatius.php
+++ asociatius.php
@@ -1,1 +1,20 @@
 <?php
+     $futbolistes = array(jugador1 => "Martin Valjent", jugador2 => "Galarreta", jugador3 => "Abdón Prats");
+     echo "Jugador 1: " . $futbolistes["jugador1"] .
+         " Jugador 2: " . $futbolistes["jugador2"] .
+         " Jugador 3: " . $futbolistes["jugador3"];
+
+     echo "<br>";
+
+     $meuArray = array(nom => "Joan", edat => 21, esAlumne => true, 24 => 4);
+
+     echo "Nom: " . $meuArray["nom"] .
+         " Edat: " . $meuArray["edat"] .
+         " Es alumne? :" . $meuArray["esAlumne"] .
+         " Pos 24: " . $meuArray[24];
+
+     echo "<br>";
+
+     var_dump($futbolistes);
+     echo "<br>";
+     var_dump($meuArray);
