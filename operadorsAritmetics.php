77a3916d8e7c4492f096766094eafe66e6ce7bda
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-04 09:27:27 +0200

    Fitxer 'operadorsAritmetics.php' amb codi

---
+++ operadorsAritmetics.php
@@ -0,0 +1,24 @@
+<?php
+    $num1 = 24;
+    $num2 = 4;
+
+    echo $num1 . " + " . $num2 . " = " . $num1 + $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " - " . $num2 . " = " . $num1 - $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " x " . $num2 . " = " . $num1 * $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " / " . $num2 . " = " . $num1 / $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " elevat a " . $num2 . " = " . $num1 ** $num2;
+    echo "<p> </p>";
+
+    echo $num1 . " / " . $num2 . " = " . "Residu: " . $num1 * $num2;
+    echo "<p> </p>";
+
+
+?>
