82569387ea808b287f3d5f3a0e317c2f64bb0e2a
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-04 09:51:28 +0200

    Afegits salts de linia a 'operadorsComparcio.php'

--- operadorsComparcio.php
+++ operadorsComparcio.php
@@ -4,33 +4,41 @@
 
     if ($num1 == $num2){
         echo "num1 és igual a num2";
+        echo "<p> </p>";
     }
 
     if ($num1 === $num2){
         echo "num1 és idèntic a num2";
+        echo "<p> </p>";
     }
 
     if ($num1 != $num2){
         echo "num1 és diferent a num2";
+        echo "<p> </p>";
     }
 
     if ($num1 !== $num2){
         echo "num1 no és identic a num2";
+        echo "<p> </p>";
     }
 
     if ($num1 > $num2){
         echo "num1 és major que num2";
+        echo "<p> </p>";
     }
 
     if ($num1 < $num2){
         echo "num1 és menor que num2";
+        echo "<p> </p>";
     }
 
     if ($num1 >= $num2){
         echo "num1 és major o igual que num2";
+        echo "<p> </p>";
     }
 
     if ($num1 <= $num2){
         echo "num1 és menor o igual que num2";
+        echo "<p> </p>";
     }
 ?>
