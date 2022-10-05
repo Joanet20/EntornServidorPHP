8fcd5c2d3ef92dc37540215d5e33de7ac1fe9bd7
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-05 08:29:57 +0200

    Fitxer 'operadorsIncrDecr.php' amb codi

---
+++ operadorsIncrDecr.php
@@ -0,0 +1,35 @@
+<?php
+    $num1 = 24;
+    $num2 = 4;
+    $num3 = 9;
+    $num4 = 18;
+
+    echo "Valor de num1: " . $num1;
+    echo "<p> </p>";
+    echo "Valor de num2: " . $num2;
+    echo "<p> </p>";
+    echo "Valor de num3: " . $num3;
+    echo "<p> </p>";
+    echo "Valor de num4: " . $num4;
+    echo "<p> </p>";
+
+    echo "num1++ => " . $num1++;
+    echo "<p> </p>";
+    echo "num1: " . $num1;
+    echo "<p> </p>";
+
+    echo "++num2 => " . ++$num2;
+    echo "<p> </p>";
+    echo "num2: " . ++$num2;
+    echo "<p> </p>";
+
+    echo "num3-- => " . $num3--;
+    echo "<p> </p>";
+    echo "num3: " . $num3;
+    echo "<p> </p>";
+
+    echo "--num4 => " . --$num4;
+    echo "<p> </p>";
+    echo "num4: " . $num4;
+    echo "<p> </p>";
+?>
