d9e3bbd767e6bbdb7a0afcf689419145404290da
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-11 08:32:35 +0200

    Arxiu 'dowhile.php' amb codi

---
+++ dowhile.php
@@ -0,0 +1,8 @@
+<?php
+    $valor = 10;
+
+    do {
+        //Es do s executa almenos un pic, siguiendo vertadera o falsa sa condicio!!
+        echo "Esteim dins es 'do'";
+    } while ($valor != 10)
+?>
