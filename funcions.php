9b416dd6abf0f010db0cf28e0c0ca1428d474e53
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-11 09:31:46 +0200

    Arxiu 'funcions.php' amb codi

---
+++ funcions.php
@@ -0,0 +1,29 @@
+<?php
+
+    $valor1 = 24;
+    $valor2 = 4;
+
+    //Funcio sense retorn ni paràmetres
+    function suma(){
+        echo "Funcio suma()";
+        echo "<p> </p>";
+    }
+
+    suma();
+
+    //Funcio amb paràmetres sense retorn
+    function sumaPar($v1, $v2){
+        echo "Resultat: " . $v1 + $v2;
+        echo "<p> </p>";
+    }
+
+    sumaPar($valor1, 9);
+
+
+    //Funcio amb paràmetres i retorn
+    function sumaRet($v1, $v2){
+        return $v1 + $v2;
+    }
+
+    sumaRet(21, $valor2);
+?>
