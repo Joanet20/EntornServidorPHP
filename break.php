a2ccde981419ebf536e5a462f085573db41114ef
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-11 09:17:15 +0200

    Afegits salts de linia a 'break.php'

--- break.php
+++ break.php
@@ -4,12 +4,14 @@ $limit = 17;
     // For normal
     for($i = 0; $i < $limit; $i++){
         echo "Valor de i: " . $i;
+        echo "<p> </p>";
     }
 
     //Amb es break a nes moment k $i es 3 acaba es bucle
 
     for($i = 0; $i < $limit; $i++){
         echo "Valor de i: " . $i;
+        echo "<p> </p>";
         if($i == 3) {
             break;
             //S atura es bucle
@@ -19,8 +21,10 @@ $limit = 17;
     //Fa que acabin es 2 fors
     for($i = 0; $i < $limit; $i++){
         echo "Valor de i: " . $i;
+        echo "<p> </p>";
         for($j = 0; $j < $limit; $j++){
             echo "Valor de j: " . $j;
+            echo "<p> </p>";
             break;
         }
     }
