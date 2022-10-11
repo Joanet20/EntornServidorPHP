fb1be361d5ed6a8cae0012d6a8ff31757d1c2a1d
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-11 08:48:33 +0200

    Arxiu 'include.php' amb codi

---
+++ include.php
@@ -0,0 +1,11 @@
+<?php
+  echo "Fitxer include.php";
+
+  //Amb include podem incloure contingut d'un altre fitxer, també el podem incloure tots es pics que escriguem include
+    include "while.php";
+    include "while.php";
+
+    //Amb include_once només permetrà inclourerlo una vegada
+    include "dowhile.php";
+    include_once "dowhile.php";
+?>
