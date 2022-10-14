7965930828216d536f5b5dd0c8476fc7b659f278
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-14 18:55:31 +0200

    Corregit error de sintaxis amb es nom des fitxer

--- PHPAmpliandoConceptos/escrituraFitxers.php
+++ PHPAmpliandoConceptos/escrituraFitxers.php
@@ -1,12 +1,12 @@
 <?php
 $contF1 = fopen("file2.txt", "a+");
 
-$contF2 = fread($contF1, filesize("file1.txt"));
+$contF2 = fread($contF1, filesize("file2.txt"));
 echo $contF2 . "<br>";
 
 //Escriu una nova linia
 fwrite($contF1, "Nova linia");
-$contF3 = fread($contF1, filesize("file1.txt"));
+$contF3 = fread($contF1, filesize("file2.txt"));
 echo $contF3 . "<br>";
 
 
