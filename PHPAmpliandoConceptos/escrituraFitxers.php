9d230722465ab5a200cac9b57c2c722173f28a1c
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-14 18:52:51 +0200

    Arxiu 'escrituraFitxers.php' amb codi

---
+++ PHPAmpliandoConceptos/escrituraFitxers.php
@@ -0,0 +1,13 @@
+<?php
+$contF1 = fopen("file2.txt", "a+");
+
+$contF2 = fread($contF1, filesize("file1.txt"));
+echo $contF2 . "<br>";
+
+//Escriu una nova linia
+fwrite($contF1, "Nova linia");
+$contF3 = fread($contF1, filesize("file1.txt"));
+echo $contF3 - "<br>";
+
+
+?>
