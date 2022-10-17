e086164608814e4e5ad7dedae6201f74fa07aef4
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-17 13:55:26 +0200

    Arxiu 'funcionsVaries.php' amb codi

---
+++ PHPAmpliandoConceptos/funcionsVaries.php
@@ -0,0 +1,41 @@
+<?php
+
+//abs() treu es valor absolut
+echo abs(2.4);
+echo "<br>";
+
+//max() mostra es valor més gran de una serie
+echo max(2, 24, 7, 21);
+echo "<br>";
+
+
+//min() mostra es valor més petit de una serie
+echo min(2, 24, 7, 21);
+echo "<br>";
+
+//rand() treu un número aleatori i li podem indicar un rang
+echo rand(2, 50);
+echo "<br>";
+
+echo rand();
+echo "<br>";
+
+
+//sqrt() mostra s'arrel quadrada d'un numero
+echo sqrt(24);
+echo "<br>";
+
+//Aquestes funcions nos permeten cambiar es tipus de numeració
+
+echo bindec("24");
+echo "<br>";
+
+echo decbin("011001");
+echo "<br>";
+
+echo hexdec("24");
+echo "<br>";
+
+echo dechex("24");
+echo "<br>";
+?>
