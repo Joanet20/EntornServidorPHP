d7fb279abc1778703b646b38eede996b58e90e6a
Joan Vallbona <johnyvallbona@gmail.com>
2022-10-17 16:00:08 +0200

    Pendent if amb cookies

--- PHPAmpliandoConceptos/crearEliminarCookies.php
+++ PHPAmpliandoConceptos/crearEliminarCookies.php
@@ -2,13 +2,13 @@
 //setcookie() serveix per crear una cookie
 
 setcookie("noexpira", "prova");
-setcookie("unset", "prova");
-setcookie("expira", "provaExp", time() + 10);
+setcookie("unset", "us");
+setcookie("expira", "provaExp", time() + 30);
 
-if ($_COOKIE['noexpira'] == "prova"){
+/*if ($_COOKIE['noexpira'] == "prova"){
     echo "Axo es sa cookie no expira";
     echo "<br>";
-}
+}*/
 
 //Per borrar cookies podem emplear sa funció unset() o posarli una data d'expiració pasada
 unset($_COOKIE['unset']);
